e06bda8fc0713dba83d7a07102bb9d1c54227a36
Rajveer Sharma Python <rajveersharma.python@gmail.com>
2026-02-02 19:48:34 +0530

    added py. files (1 to 5)

---
+++ 02_Table_Of_2.py
@@ -0,0 +1,7 @@
+#
+
+multiplier = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for multiplicand in multiplier:
+    txt = "2"
+    result = int(txt) * multiplicand
+    print(txt, "*", multiplicand, "=", result)
