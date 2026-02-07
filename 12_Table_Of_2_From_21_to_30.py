5319e606f50978ff18d94fef3154e1eef3963d51
Rajveer Sharma Python <rajveersharma.python@gmail.com>
2026-02-07 20:40:38 +0530

    1st commit

---
+++ 12_Table_Of_2_From_21_to_30.py
@@ -0,0 +1,6 @@
+# FOR rajeevsharma3010-glitch.
+multiplier = [21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+for multiplicand in multiplier:     
+    txt = "2"
+    result = int(txt) * multiplicand
+    print(txt, "*", multiplicand, "=", result)
